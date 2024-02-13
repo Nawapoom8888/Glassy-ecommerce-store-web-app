2c8306b79dee53ac34995c854b024145749d2379
Nawapoom Panichwilai <nawapoom.panichwilai@gmail.com>
2024-02-14 00:12:16 +0700

    fixed code in backend

--- backend/index.js
+++ backend/index.js
@@ -32,10 +32,6 @@ app.use(express.urlencoded({ extended: true }));
 // Cookie Parser Middleware
 app.use(cookieParser());
 
-app.get("/", (req, res, next) => {
-  res.send("API is running...");
-});
-
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
