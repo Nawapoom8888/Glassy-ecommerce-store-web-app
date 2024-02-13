a252cfc8320b21b75e1f3b4d992008ce726e39d2
Nawapoom Panichwilai <nawapoom.panichwilai@gmail.com>
2024-02-13 22:28:30 +0700

    config value

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), autoprefixer(), tailwindcss()],
+  plugins: [react()],
   server: {
     proxy: {
       "/api": {
